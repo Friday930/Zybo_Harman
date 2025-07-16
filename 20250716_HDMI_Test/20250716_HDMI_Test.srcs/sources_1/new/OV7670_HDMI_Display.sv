4e2be0817ad1f51da71c705cf3b13241ddd763e4
Friday930 <tukankr@khu.ac.kr>
2025-07-16 09:51:53 +0900

    first commit

---
+++ 20250716_HDMI_Test/20250716_HDMI_Test.srcs/sources_1/new/OV7670_HDMI_Display.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/07/16 09:50:47
+// Design Name: 
+// Module Name: OV7670_HDMI_Display
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module OV7670_HDMI_Display(
+
+    );
+endmodule
